173076b58c25d22826aecd7f7de51f945fe19c2c
roman.denysenko <roman.denysenkoo@gmail.com>
2022-02-15 17:30:25 +0200

    my first commit

---
+++ src/test/java/MyTest/MyTest.java
@@ -0,0 +1,15 @@
+package MyTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void sumTest () {
+
+        Assert.assertTrue("Test failed", true);
+
+
+    }
+}
